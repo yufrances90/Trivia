65d4f075bb29a61364dfdcb9ce6b2ad5fac93564
yufrances90 <yufrances90@gmail.com>
2020-03-31 16:47:45 -0700

    fix: Housekeeping

--- app/backend/flaskr/__init__.py
+++ app/backend/flaskr/__init__.py
@@ -91,17 +91,8 @@ def create_app(test_config=None):
       'success': -1
     })
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
-
+  
   '''
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
@@ -111,6 +102,15 @@ def create_app(test_config=None):
   TEST: Search by any phrase. The questions list will update to include 
   only question that include that string within their question. 
   Try using the word "title" to start. 
+
+  @TODO: 
+  Create an endpoint to POST a new question, 
+  which will require the question and answer text, 
+  category, and difficulty score.
+
+  TEST: When you submit a question on the "Add" tab, 
+  the form will clear and the question will appear at the end of the last page
+  of the questions list in the "List" tab.  
   '''
   @app.route('/questions', methods=["POST"])
   def search_questions():
@@ -150,28 +150,6 @@ def create_app(test_config=None):
   categories in the left column will cause only questions of that 
   category to be shown. 
   '''
-
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
-
-
-  '''
-    OTHER ENDPOINTS
-  '''
-
-  '''
-    Get questions by a given category id
-  '''
   @app.route('/categories/<int:category_id>/questions')
   def get_questions_by_category(category_id):
 
@@ -186,6 +164,19 @@ def create_app(test_config=None):
     }
 
     return jsonify(res)
+
+
+  '''
+  @TODO: 
+  Create a POST endpoint to get questions to play the quiz. 
+  This endpoint should take category and previous question parameters 
+  and return a random questions within the given category, 
+  if provided, and that is not one of the previous questions. 
+
+  TEST: In the "Play" tab, after a user selects "All" or a category,
+  one question at a time is displayed, the user is allowed to answer
+  and shown whether they were correct or not. 
+  '''
 
   '''
   @TODO: 
