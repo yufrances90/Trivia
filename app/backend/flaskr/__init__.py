4859f463e3c93561b351645b0d19bc8611a89a40
yufrances90 <yufrances90@gmail.com>
2020-03-31 17:55:36 -0700

    feat: Housekeeping(2)

--- app/backend/flaskr/__init__.py
+++ app/backend/flaskr/__init__.py
@@ -34,6 +34,7 @@ def create_app(test_config=None):
   def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
   '''
   @TODO: 
